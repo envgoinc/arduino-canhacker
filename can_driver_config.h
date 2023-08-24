464766538f045c8064ae7330929a7b949b47727f
Jugal Bilimoria <jugalbili@yahoo.ca>
2023-08-24 14:29:16 -0400

    Fix to work with STM CanAlyzer Board

--- can_driver_config.h
+++ can_driver_config.h
@@ -3,7 +3,9 @@
 
 // Comment out the driver that is not being used 
 
+#if !defined(MCP_DRIVER) || !defined(STM_DRIVER)
 #define MCP_DRIVER 1
 //#define STM_DRIVER 1
+#endif // MCP_DRIVER) || STM_DRIVER
 
 #endif /* CAN_DRIVER_CONFIG_H */
